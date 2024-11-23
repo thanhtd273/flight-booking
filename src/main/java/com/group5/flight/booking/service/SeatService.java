15538ebbacd93a1c02b5058f6b88eaebeb767c42
MinThe2612 <leminthe01@gmail.com>
2024-11-24 02:47:21 +0700

    Create SeatService.java

---
+++ src/main/java/com/group5/flight/booking/service/SeatService.java
@@ -0,0 +1,26 @@
+package com.group5.flight.booking.service;
+
+import com.group5.flight.booking.core.ErrorCode;
+import com.group5.flight.booking.core.exception.LogicException;
+import com.group5.flight.booking.dto.SeatInfo;
+import com.group5.flight.booking.model.Seat;
+
+import java.util.List;
+
+public interface SeatService {
+    List<Seat> getAllSeats();
+    
+    List<Seat> getSeatsByFlight(Long flightId) throws LogicException;
+
+    Seat findBySeatId(Long id) throws LogicException;
+    
+    Seat create(SeatInfo seatInfo) throws LogicException;
+
+    Seat update(Long id, SeatInfo seatInfo) throws LogicException;
+
+    ErrorCode delete(Long id) throws LogicException;
+
+    List<Seat> getAvailableSeats();
+
+    void updateSeatStatus(Long id, Boolean isAvailable) throws LogicException;
+}
