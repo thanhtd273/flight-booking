1d582ebb1a65b91ae62a8c02aeff8714871822d0
tantai27 <votantai0207@gmail.com>
2024-12-10 17:29:26 +0700

    Change Application and remove main in view

--- src/main/java/com/group5/flight/booking/Application.java
+++ src/main/java/com/group5/flight/booking/Application.java
@@ -1,13 +1,112 @@
 package com.group5.flight.booking;
 
+import com.group5.flight.booking.view.component.PanelCover;
+import com.group5.flight.booking.view.component.PanelLoginAndRegister;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.text.DecimalFormat;
+import java.text.DecimalFormatSymbols;
+import java.util.Locale;
+import net.miginfocom.swing.MigLayout;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.jdesktop.animation.timing.Animator;
+import org.jdesktop.animation.timing.TimingTarget;
+import org.jdesktop.animation.timing.TimingTargetAdapter;
 
 @SpringBootApplication
-public class Application {
+public class Application extends javax.swing.JFrame{
+
+    private final DecimalFormat df = new DecimalFormat("##0.###", DecimalFormatSymbols.getInstance(Locale.US));
+    private MigLayout layout;
+    private PanelCover cover;
+    private PanelLoginAndRegister loginAndRegister;
+    private boolean isLogin = true;
+    private final double addSize = 30;
+    private final double coverSize = 40;
+    private final double loginSize = 60;
+
+    public Application() {
+        initComponents();
+        init();
+    }
+
+    private void init() {
+        layout = new MigLayout("fill, insets 0");
+        cover = new PanelCover();
+        loginAndRegister = new PanelLoginAndRegister();
+        TimingTarget target = new TimingTargetAdapter() {
+            @Override
+            public void timingEvent(float fraction) {
+                double fractionCover;
+                double fractionLogin;
+                double size = coverSize;
+                if (fraction <= 0.5f) {
+                    size += fraction * addSize;
+                } else {
+                    size += addSize - fraction * addSize;
+                }
+                if (isLogin) {
+                    fractionCover = 1f - fraction;
+                    fractionLogin = fraction;
+                    if (fraction >= 0.5f) {
+                        cover.registerRight(fractionCover * 100);
+                    } else {
+                        cover.loginRight(fractionLogin * 100);
+                    }
+                } else {
+                    fractionCover = fraction;
+                    fractionLogin = 1f - fraction;
+                    if (fraction <= 0.5f) {
+                        cover.registerLeft(fraction * 100);
+                    } else {
+                        cover.loginLeft((1f - fraction) * 100);
+                    }
+                }
+                if (fraction >= 0.5f) {
+                    loginAndRegister.showRegister(isLogin);
+                }
+                fractionCover = Double.valueOf(df.format(fractionCover));
+                fractionLogin = Double.valueOf(df.format(fractionLogin));
+                layout.setComponentConstraints(cover, "width " + size + "%, pos " + fractionCover + "al 0 n 100%");
+                layout.setComponentConstraints(loginAndRegister, "width " + loginSize + "%, pos " + fractionLogin + "al 0 n 100%");
+                bg.revalidate();
+            }
+
+            @Override
+            public void end() {
+                isLogin = !isLogin;
+            }
+        };
+        Animator animator = new Animator(800, target);
+        animator.setAcceleration(0.5f);
+        animator.setDeceleration(0.5f);
+        animator.setResolution(0);  //  for smooth animation
+        bg.setLayout(layout);
+        bg.add(cover, "width " + coverSize + "%, pos " + (isLogin ? "1al" : "0al") + " 0 n 100%");
+        bg.add(loginAndRegister, "width " + loginSize + "%, pos " + (isLogin ? "0al" : "1al") + " 0 n 100%"); //  1al as 100%
+        loginAndRegister.showRegister(!isLogin);
+        cover.login(isLogin);
+        cover.addEvent(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent ae) {
+                if (!animator.isRunning()) {
+                    animator.start();
+                }
+            }
+        });
+    }
+
+    @SuppressWarnings("unchecked")
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
-	}
-
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Main().setVisible(true);
+            }
+        });
+    }
+        private javax.swing.JLayeredPane bg;
 }
